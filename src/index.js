6c28da0ac34e795136c58be72762b3cbdc365dbd
Devon Walsh <devonwalsh@gmail.com>
2021-03-23 23:50:49 -0400

    General formatting

--- src/index.js
+++ src/index.js
@@ -12,58 +12,58 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+});
 
-  document.querySelector(".add-toy-form").addEventListener("submit", () => {
-    event.preventDefault();
-    let newToy = {};
-    let formData = document.querySelectorAll(".input-text")
-    formData.forEach( element => {
-      newToy[element.name] = element.value;
-    })
-    newToy.likes = 0;
-
-    const configObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(newToy)
-    }; 
-    
-    fetch("http://localhost:3000/toys", configObj)
-
-    addCards(newToy);
+document.querySelector(".add-toy-form").addEventListener("submit", () => {
+  event.preventDefault();
+  let newToy = {};
+  let formData = document.querySelectorAll(".input-text");
+  formData.forEach( element => {
+    newToy[element.name] = element.value;
   });
-
-  fetch("http://localhost:3000/toys")
-  .then(response => response.json())
-  .then(data => {
-    data.forEach( toy => addCards(toy))
-  })
-
-  function addCards(newToy) {
-    const toyCollection = document.getElementById("toy-collection");
-    const card = document.createElement("div");
-    const toyName = document.createElement("h2");
-    const toyImage = document.createElement("img");
-    const toyLikes = document.createElement("p");
-    const likeButton = document.createElement("button");
-
-    toyName.innerHTML = newToy.name;
-    toyImage.src = newToy.image;
-    toyImage.classList.add("toy-avatar");
-    toyLikes.innerHTML = newToy.likes;
-    likeButton.classList.add("like-btn");
-    likeButton.innerHTML = "Like <3";
-
-    card.appendChild(toyName);
-    card.appendChild(toyImage);
-    card.appendChild(toyLikes);
-    card.appendChild(likeButton);
-
-    card.classList.add("card");
-    toyCollection.appendChild(card);
-  }
+  newToy.likes = 0;
+  
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(newToy)
+  }; 
+  
+  fetch("http://localhost:3000/toys", configObj);
+  
+  addCards(newToy);
 
 });
+
+fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach( toy => addCards(toy));
+  });
+
+function addCards(newToy) {
+  const toyCollection = document.getElementById("toy-collection");
+  const card = document.createElement("div");
+  const toyName = document.createElement("h2");
+  const toyImage = document.createElement("img");
+  const toyLikes = document.createElement("p");
+  const likeButton = document.createElement("button");
+  
+  toyName.innerHTML = newToy.name;
+  toyImage.src = newToy.image;
+  toyImage.classList.add("toy-avatar");
+  toyLikes.innerHTML = newToy.likes;
+  likeButton.classList.add("like-btn");
+  likeButton.innerHTML = "Like <3";
+  
+  card.appendChild(toyName);
+  card.appendChild(toyImage);
+  card.appendChild(toyLikes);
+  card.appendChild(likeButton);
+  
+  card.classList.add("card");
+  toyCollection.appendChild(card);
+}
