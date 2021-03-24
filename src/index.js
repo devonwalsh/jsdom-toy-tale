0d82e6476cf8041c3e630be6ed190bc99cf97fef
Devon Walsh <devonwalsh@gmail.com>
2021-03-23 23:45:27 -0400

    Updated function to immediately add new toy card on form submit

--- src/index.js
+++ src/index.js
@@ -32,34 +32,38 @@ document.addEventListener("DOMContentLoaded", () => {
     }; 
     
     fetch("http://localhost:3000/toys", configObj)
+
+    addCards(newToy);
   });
 
   fetch("http://localhost:3000/toys")
   .then(response => response.json())
   .then(data => {
-    data.forEach( toy => {
-
-      const toyCollection = document.getElementById("toy-collection");
-      const card = document.createElement("div");
-      const toyName = document.createElement("h2");
-      const toyImage = document.createElement("img");
-      const toyLikes = document.createElement("p");
-      const likeButton = document.createElement("button");
-
-      toyName.innerHTML = toy.name;
-      toyImage.src = toy.image;
-      toyImage.classList.add("toy-avatar");
-      toyLikes.innerHTML = toy.likes;
-      likeButton.classList.add("like-btn");
-      likeButton.innerHTML = "Like <3";
-
-      card.appendChild(toyName);
-      card.appendChild(toyImage);
-      card.appendChild(toyLikes);
-      card.appendChild(likeButton);
-
-      card.classList.add("card");
-      toyCollection.appendChild(card);
-    })
+    data.forEach( toy => addCards(toy))
   })
+
+  function addCards(newToy) {
+    const toyCollection = document.getElementById("toy-collection");
+    const card = document.createElement("div");
+    const toyName = document.createElement("h2");
+    const toyImage = document.createElement("img");
+    const toyLikes = document.createElement("p");
+    const likeButton = document.createElement("button");
+
+    toyName.innerHTML = newToy.name;
+    toyImage.src = newToy.image;
+    toyImage.classList.add("toy-avatar");
+    toyLikes.innerHTML = newToy.likes;
+    likeButton.classList.add("like-btn");
+    likeButton.innerHTML = "Like <3";
+
+    card.appendChild(toyName);
+    card.appendChild(toyImage);
+    card.appendChild(toyLikes);
+    card.appendChild(likeButton);
+
+    card.classList.add("card");
+    toyCollection.appendChild(card);
+  }
+
 });
