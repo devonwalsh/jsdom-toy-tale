aece2df200a55fe285fea1e3b8b7cb4951d8566a
Devon Walsh <devonwalsh@gmail.com>
2021-03-25 17:31:01 -0400

    Finally figured out like incrementing for newly-added toys

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 let addToy = false;
 
+// Events run on DOM load
 document.addEventListener("DOMContentLoaded", () => {
+  addToyDropdown();
+  addToyCards();
+});
+
+// Handles "add toy" dropdown window
+function addToyDropdown() {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,9 +20,51 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+}
 
-document.querySelector(".add-toy-form").addEventListener("submit", () => {
+// Adds toy cards to page
+function addToyCards() {
+  fetch("http://localhost:3000/toys")
+    .then(response => response.json())
+    .then(data => {
+      data.forEach( toy => populateToyCards(toy));
+      //addLikeCountEventListener();
+    });
+}
+
+// Populates cards with toy data
+function populateToyCards(newToy) {
+  const toyCollection = document.getElementById("toy-collection");
+  const card = document.createElement("div");
+  const toyName = document.createElement("h2");
+  const toyImage = document.createElement("img");
+  const toyLikes = document.createElement("p");
+  const likeButton = document.createElement("button");
+  
+  card.id = newToy.id;
+  toyName.innerHTML = newToy.name;
+  toyImage.src = newToy.image;
+  toyImage.classList.add("toy-avatar");
+  toyLikes.innerHTML = newToy.likes;
+  toyLikes.classList.add("like-count");
+  likeButton.classList.add("like-btn");
+  likeButton.innerHTML = "Like <3";
+  
+  card.appendChild(toyName);
+  card.appendChild(toyImage);
+  card.appendChild(toyLikes);
+  card.appendChild(likeButton);
+  
+  card.classList.add("card");
+  toyCollection.appendChild(card);
+
+  likeButton.addEventListener("click", likeIncrementer);
+}
+
+// "Add toy" form submit behavior
+document.querySelector(".add-toy-form").addEventListener("submit", submitToyForm);
+
+function submitToyForm() {
   event.preventDefault();
   let newToy = {};
   let formData = document.querySelectorAll(".input-text");
@@ -32,38 +82,46 @@ document.querySelector(".add-toy-form").addEventListener("submit", () => {
     body: JSON.stringify(newToy)
   }; 
   
-  fetch("http://localhost:3000/toys", configObj);
-  
-  addCards(newToy);
-
-});
-
-fetch("http://localhost:3000/toys")
+  fetch("http://localhost:3000/toys", configObj)
   .then(response => response.json())
-  .then(data => {
-    data.forEach( toy => addCards(toy));
-  });
+  .then(object => populateToyCards(object));
+  
+}
 
-function addCards(newToy) {
-  const toyCollection = document.getElementById("toy-collection");
-  const card = document.createElement("div");
-  const toyName = document.createElement("h2");
-  const toyImage = document.createElement("img");
-  const toyLikes = document.createElement("p");
-  const likeButton = document.createElement("button");
+// Increments like counts
+function likeIncrementer(e) {
+  let likeCount = e.target.parentNode.querySelector(".like-count").innerHTML;
+  let id = e.target.parentNode.id;
+  likeCount++;
+  e.target.parentNode.querySelector(".like-count").innerHTML = likeCount;
   
-  toyName.innerHTML = newToy.name;
-  toyImage.src = newToy.image;
-  toyImage.classList.add("toy-avatar");
-  toyLikes.innerHTML = newToy.likes;
-  likeButton.classList.add("like-btn");
-  likeButton.innerHTML = "Like <3";
+  patchLikes(id, likeCount);
+}
+
+// Sends updated like counts to db.json
+function patchLikes(id, likeCount) {
+  const configObj = {
+    method: "PATCH",
+    headers:
+    {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": likeCount
+    })
+  };
   
-  card.appendChild(toyName);
-  card.appendChild(toyImage);
-  card.appendChild(toyLikes);
-  card.appendChild(likeButton);
+  fetch(`http://localhost:3000/toys/${id}`, configObj)
+  .then(response => response.json())
+  .then(object => console.log(object)); 
   
-  card.classList.add("card");
-  toyCollection.appendChild(card);
+}
+
+// Attaches "like incrementer" functionality to like buttons
+function addLikeCountEventListener() {
+  const likes = document.querySelectorAll(".like-btn");
+  likes.forEach( (e) => {
+    e.addEventListener("click", likeIncrementer);
+  });
 }
